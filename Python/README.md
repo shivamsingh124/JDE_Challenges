fc093caabc465a1e3419ab3432668ccfa88fcdca
toshan-luktuke <toshan1603@gmail.com>
2022-04-02 20:14:31 +0530

    Add Python Task

---
+++ Python/README.md
@@ -0,0 +1,5 @@
+## Gif of robot moving with opposite bounce i.e. the robot bounces in the opposite direction after hitting the wall
+![Opposite bounce gif](robot-particle-opposite-bounce.gif)
+
+## Gif of robot moving with random bounce i.e. the robot bounces in a random direction after hitting the wall
+![Random bounce gif](robot-particle-random-bounce.gif)
---
+++ Python/robot_particle.py
@@ -0,0 +1,53 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import animation
+import random
+
+opposite_bounce = True
+xlimit = 50
+ylimit = 50
+
+class Robot:
+    def __init__(self):
+        global xlmit, ylimit
+        self.x = xlimit/2
+        self.y = ylimit/2
+        self.vx = random.randint(1, 5)
+        self.vy = random.randint(1, 5)
+
+    def move(self):
+        if self.x >= xlimit or self.x <= 0:
+            if opposite_bounce:
+                self.vx *= -1
+            else:
+                self.vx = -1* random.random()
+                self.vy = random.random()
+
+
+        if self.y >= ylimit or self.y <= 0:
+            if opposite_bounce:
+                self.vy *= -1
+            else:
+                self.vy = -1 * random.random()
+                self.vx = random.random()
+
+        
+        self.x += self.vx
+        self.y += self.vy
+
+def animate(frame_number):
+    print('frame_number:', frame_number)
+
+    robot.move()
+
+    d.set_data([robot.x],[robot.y])
+
+robot = Robot()
+
+fig = plt.gcf()
+ax = plt.axes(xlim=(0,xlimit), ylim=(0,ylimit))
+d, = plt.plot(robot.x, robot.y, 'go')
+anim = animation.FuncAnimation(fig, animate, frames=200, interval=10, repeat=True)
+
+anim.save("robot-particle-opposite-bounce.gif", fps=24)
